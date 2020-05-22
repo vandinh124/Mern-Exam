35399ab26af4c8a2287d8572bdb53247bf4c9abf
Van Dinh <vanvdinh124@gmail.com>
2020-05-22 14:06:03 -0700

    fixed missed step

---
+++ client/build/precache-manifest.a93a1fe314191fbde6e8d25b996eafe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb860ba6b71b7bd13385071ac7442457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7487bb66855b0001ae9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2610f80846b9f09a36e9",
+    "url": "/static/js/2.cd01beee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd01beee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7487bb66855b0001ae9",
+    "url": "/static/js/main.d1bb79eb.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
